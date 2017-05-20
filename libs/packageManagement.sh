2711d035031f99ed52560ed862922122c6338731
Kevin Sandom <kevinsandom@yahoo.co.uk>
2017-05-20 14:55:57 +0100

    Fix difficult dependencies, so they work the first time.

--- libs/packageManagement.sh
+++ libs/packageManagement.sh
@@ -6,5 +6,8 @@ function packages
     echo "Packages: Will install: $@"
     apt-get update
     apt-get install -y "$@"
+    
+    # This magically fixes dependencies that could not be resolved the first time around.
     apt-get --fix-broken install -y
+    apt-get install -y "$@"
 }
