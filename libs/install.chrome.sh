555fac3f72f5d60de1a9aefbe0d66b6c7b23c3a7
Kevin Sandom <kevinsandom@yahoo.co.uk>
2017-05-20 14:58:57 +0100

    Separated chrome prereqs handeling for future use.

--- libs/install.chrome.sh
+++ libs/install.chrome.sh
@@ -5,8 +5,13 @@ packagesChromePrereqs="libappindicator1"
 function preFetchChrome
 {
     if ! which google-chrome > /dev/null; then
-        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+        fetchChrome
     fi
+}
+
+function fetchChrome
+{
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 }
 
 function installChrome
