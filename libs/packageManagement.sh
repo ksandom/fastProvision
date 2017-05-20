44edbdfa472c0c854d6f21b1f3a43e09b136a996
Kevin Sandom <kevinsandom@yahoo.co.uk>
2017-05-20 14:05:22 +0100

    Added comment about package management.

--- libs/packageManagement.sh
+++ libs/packageManagement.sh
@@ -1,4 +1,5 @@
 # This contains the functions to manage the packages.
+# The idea of this it to let apt, which is a very mature package manager, do it's stuff in one go, rather than micro-managing it all the way.
 
 function packages
 {
