6b84f2d0410d5786a6e6cd93892bf09f62d210f4
Kevin Sandom <kjsandom@gmail.com>
2017-11-24 23:00:01 +0000

    Abstracted desktop into debian and devuan variants.

--- libs/packages.desktop.sh
+++ libs/packages.desktop.sh
@@ -1,3 +1,5 @@
 # This file contains packages I use for day-to-day desktop usage.
 
-packagesDesktop="firefox firefox-locale-en kruler"
+packagesDesktop="kruler"
+packagesDesktopUbuntu="firefox firefox-locale-en $packagesDesktop"
+packagesDesktopDevuan="firefox-esr firefox-esr-l10n-uk $packagesDesktop"
