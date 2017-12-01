5fb4b7e7e90d78a2970d0e3ea7551ad7042c95bc
Kevin Sandom <kevinsandom@yahoo.co.uk>
2017-12-01 18:32:30 +0000

    Early stage development of a gentoo chroot image.

---
+++ libs/chrootGentoo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Build a gentoo chroot.
+
+chrootGentooStage3URL="http://distfiles.gentoo.org/releases/arm/autobuilds/current-stage3-armv6j_hardfp/"
+chrootGentooCache="/tmp"
+
+function chrootGentooGetTarball
+{
+	echo "Get the stage3 tarball."
+	stage3Name="`chrootGentooGetTarballName`"
+	export stage3CacheName="$chrootGentooCache/$stage3Name"
+	
+	wget --continue "$chrootGentooStage3URL/$stage3Name" -O "$stage3CacheName"
+}
+
+function chrootGentooGetTarballName
+{
+	curl "$chrootGentooStage3URL" | grep 'href="stage' | sed 's/^.*href=//g' | cut -d\" -f2 | grep '\.bz2$'
+}
+
+function chrootGentooExtractTarball
+{
+	echo "Extract the tarball."
+	tar -C "$mountPoint" -xjf "$stage3CacheName"
+}
+
+function chrootGentooBuildAll
+{
+	chrootPrerequisites
+	
+	chrootCreateBareImage
+	chrootMountImage
+	
+	chrootGentooGetTarball
+	
+	chrootGentooExtractTarball
+	
+}
+
