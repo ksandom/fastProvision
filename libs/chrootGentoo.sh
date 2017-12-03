4b7945e52505676e1540138ba550d3368e867e99
Kevin Sandom <kevinsandom@yahoo.co.uk>
2017-12-03 12:29:53 +0000

    Gentoo: Progress getting gentoo to work.

--- libs/chrootGentoo.sh
+++ libs/chrootGentoo.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 # Build a gentoo chroot.
 
-chrootGentooStage3URL="http://distfiles.gentoo.org/releases/arm/autobuilds/current-stage3-armv6j_hardfp/"
 chrootGentooCache="/tmp"
+chrootGentooStage3URL="http://distfiles.gentoo.org/releases/arm/autobuilds/current-stage3-armv6j_hardfp/"
+chrootGentooPortageURL="http://distfiles.gentoo.org/snapshots/"
+chrootGentooPortageFile="portage-latest.tar.bz2"
+
+function chrootGentooPrerequisites
+{
+	export chrootGentooPortageFullURL="$chrootGentooPortageURL$chrootGentooPortageFile"
+	export chrootGentooPortageFullFile="$chrootGentooCache/$chrootGentooPortageFile"
+}
 
 function chrootGentooGetTarball
 {
@@ -18,22 +26,57 @@ function chrootGentooGetTarballName
 	curl "$chrootGentooStage3URL" | grep 'href="stage' | sed 's/^.*href=//g' | cut -d\" -f2 | grep '\.bz2$'
 }
 
+function chrootGentooGetPortage
+{
+	echo "Get portage."
+	wget --continue "$chrootGentooPortageFullURL" -O "$chrootGentooPortageFullFile"
+}
+
 function chrootGentooExtractTarball
 {
 	echo "Extract the tarball."
 	tar -C "$mountPoint" -xjpf "$stage3CacheName"
 }
 
+function chrootGentooExtractPortage
+{
+	echo "Extract portage."
+	tar -C "$mountPoint/usr" -xjpf "$chrootGentooPortageFullFile"
+}
+
+function chrootGentooPlaceHardCodedConfigs
+{
+	echo "Place hard coded configs."
+	cp -R usefulStuff/gentoo/* "$mountPoint"
+}
+
 function chrootGentooBuildAll
 {
+	# Get config and directories sorted.
 	chrootPrerequisites
+	chrootGentooPrerequisites
 	
+	# Create the blank image.
 	chrootCreateBareImage
 	chrootMountImage
 	
+	# Get starting point.
 	chrootGentooGetTarball
+	chrootGentooGetPortage
 	
+	# Place the initial contents.
 	chrootGentooExtractTarball
+	chrootGentooExtractPortage
 	
+	# Configure.
+	chrootGentooPlaceHardCodedConfigs
+	
+	# Build build tools.
+	
+	# Retrieve all packages.
+	
+	# Build all pacakges.
+	
+	# Cleanup.
 }
 
